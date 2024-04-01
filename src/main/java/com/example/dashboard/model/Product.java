d3e061e7619e3cff2c90b84e9561bf01f4035f8c
Breno Flaubert de Souza <brenoflaubert@gmail.com>
2024-04-01 10:40:58 -0300

    refactor (model product): update getters and setters using lombook and create new attributes

--- src/main/java/com/example/dashboard/model/Product.java
+++ src/main/java/com/example/dashboard/model/Product.java
@@ -5,9 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "products")
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,46 +19,19 @@ public class Product {
     private String name;
     private int price;
     private String condition;
-
-    public Product() {}
-
-    public Product(Long id, String name, int price, String condition) {
+    private int quantityPurchased;
+    private int soldAmount;
+    private int quantityInStock;
+    public Product() {
+    }
+    public Product(Long id, String name, int price, String condition, int quantityPurchased, int soldAmount, int quantityInStock) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.condition = condition;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public String getCondition() {
-        return condition;
-    }
-
-    public void setCondition(String condition) {
-        this.condition = condition;
+        this.quantityPurchased = quantityPurchased;
+        this.soldAmount = soldAmount;
+        this.quantityInStock = quantityInStock;
     }
     
 }
